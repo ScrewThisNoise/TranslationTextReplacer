968d9be3c82e200679dcf5a3ebaf85e5839feb83
ScrewThisNoise <51236954+ScrewThisNoise@users.noreply.github.com>
2020-08-13 17:55:17 +0200

    Initial commit

---
+++ TranslationTextReplacer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace TranslationTextReplacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sDir = "translation";
+
+            if (Directory.Exists("newTLtranslation"))
+                Directory.Delete("newTLtranslation", true);
+
+            foreach (string TranslationFile in Directory.EnumerateFiles(sDir, "*.txt", SearchOption.AllDirectories))
+            {
+                string[] TranslationLine = File.ReadAllLines(TranslationFile);
+                string TranslationOutputDir = "newTL" + TranslationFile.Remove(TranslationFile.Length-15,15);
+                string TranslationOutputFile = TranslationOutputDir + "translation.txt";
+
+                Console.WriteLine(TranslationOutputFile);
+                foreach (string TranslationSentence in TranslationLine)
+                {
+                    string[] splittrans = TranslationSentence.Split('=');
+                    Directory.CreateDirectory(TranslationOutputDir);
+                    try
+                    {
+                        splittrans[1] = runfix(splittrans[1]);
+                        string donetrans = splittrans[0] + "=" + splittrans[1];
+
+                        using (StreamWriter hit = new StreamWriter(TranslationOutputFile, true))
+                        {
+                            hit.WriteLine(splittrans[0] + "=" + splittrans[1]);
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        Console.Out.WriteLine(err);
+                    }
+                }
+            }
+
+        }
+
+        public static string runfix(string translation)
+        {
+            string replacer = translation;
+            replacer = replacer.Replace("…", "...");
+            //replacer = replacer.Replace("..", "...");
+            //replacer = replacer.Replace("....", "...");
+
+            replacer = Regex.Replace(replacer, @"(^|[^\.])(\.{3})+(\.{1,2})([^\.]|$)", "$1$2$4");
+            replacer = Regex.Replace(replacer, @"[\s](?<!\.)(\.{3})+(?!\.)([\S]|$)", "$2$3 $4").TrimEnd();
+            replacer = Regex.Replace(replacer, @"(^|[^\.])(\.{2})([^\.]|$)", "$1$2.$3");
+            replacer = Regex.Replace(replacer, "[ ]{2,}", " ");
+
+            return replacer;
+        }
+    }
+}
